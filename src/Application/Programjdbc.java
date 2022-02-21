21b1cac4ba19eb786871d7dfd09b2e506a29c3cc
JaimeDev <jaimegaudencio@gmail.com>
2022-02-21 20:07:16 -0300

    Seller entity class

--- src/Application/Programjdbc.java
+++ src/Application/Programjdbc.java
@@ -1,12 +1,17 @@
 package Application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Programjdbc {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
